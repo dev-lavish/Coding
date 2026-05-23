7d62b5e9b7343e4f316e4206e4161c53e64b0329
Lavish Pandir <lavishpandir0708@gmail.com>
2026-05-23 18:11:58 +0530

    Added numpy folder

--- python/PYbasics/hilo.py
+++
@@ -1,6 +0,0 @@
-# documentation of the print function
-#print(*objects, sep=' ', end='\n', file=sys.stdout, flush=False)
-x=round(input("what's the value of x ?"))
-y=round(input("what's the value of y ?"))
-z=(x+y)
-print(z)
